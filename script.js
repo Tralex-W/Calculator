22d37f851056c9ae48e5d9cece8ebf515df1d258
Eric <67967535+Tralex-W@users.noreply.github.com>
2021-04-04 22:07:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Import Items
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousText = document.querySelector('[data-previous]')
+const currentText = document.querySelector('[data-current]')
+
+
+//More code coming soon
+
